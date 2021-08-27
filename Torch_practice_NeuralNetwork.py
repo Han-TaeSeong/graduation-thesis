1afec95cfa58b4d792891659f94f4780971334dc
Han-TaeSeong <80900070+Han-TaeSeong@users.noreply.github.com>
2021-08-27 18:01:10 +0900

    Update Torch_practice_NeuralNetwork.py

--- Torch_practice_NeuralNetwork.py
+++ Torch_practice_NeuralNetwork.py
@@ -18,10 +18,12 @@ from torch.utils.data import TensorDataset
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1/7, random_state=0)
+from sklearn.model_selection import train_test_split
+
 device = torch.device("cuda")
 torch.manual_seed(777)
 torch.cuda.manual_seed_all(777)
-
 
 ########### 단층 퍼셉트론
 # def OR_gate(x1, x2):
@@ -101,3 +103,210 @@ torch.cuda.manual_seed_all(777)
 #     print(accuracy)
 
 ####### 다층 퍼셉트론 파이토치로 구현(xor)
+# X = torch.FloatTensor([[0, 0], [0, 1], [1, 0], [1, 1]]).to(device)
+# Y = torch.FloatTensor([[0], [1], [1], [0]]).to(device)
+#
+# model = nn.Sequential(nn.Linear(2, 10), nn.Sigmoid(), nn.Linear(10, 10), nn.Sigmoid(),
+#                       nn.Linear(10, 10), nn.Sigmoid(), nn.Linear(10, 1), nn.Sigmoid()).to(device)
+#
+# criterion = nn.BCELoss().to(device)
+# optimizer = optim.SGD(model.parameters(), lr=1)
+#
+# for epoch in range(10001):
+#     hypothesis = model(X)
+#     cost = criterion(hypothesis, Y)
+#
+#     optimizer.zero_grad()
+#     cost.backward()
+#     optimizer.step()
+#
+#     if epoch % 500 == 0:
+#         print(epoch, cost.item())
+#
+# with torch.no_grad():
+#     hypothesis = model(X)
+#     predicted = (hypothesis>0.5).float()
+#     accuracy = (predicted == Y).float().mean()
+#
+#     print(f'Hypothesis : {hypothesis.detach().cpu().numpy()}')
+#     print(f'Predicted : {predicted.detach().cpu().numpy()}')
+#     print(f'Y : {Y}')
+#     print(f'Accuracy : {accuracy}')
+
+
+########## 비선형 활성화 함수 (Activation Function)
+# x=torch.arange(-5,5,0.1)
+# y=torch.tanh(x)
+#
+# plt.plot(x,y)
+# plt.axvline(x=0,color='orange',linestyle=':')
+# plt.axhline(y=0,color='orange',linestyle="--")
+# plt.title('Tanh Fucntion')
+# plt.show()
+
+# def relu(x):
+#     return np.maximum(0,x)
+# x=torch.arange(-5,5,0.1)
+# y=relu(x)
+#
+# plt.plot(x,y)
+# plt.axvline(x=0,color='orange',linestyle=':')
+# plt.title('Relu Function')
+# plt.show()
+
+
+# def leakyrelu(x):
+#     return np.maximum(0.1 * x, x)
+#
+#
+# x = torch.arange(-5, 5, 0.1)
+# y = leakyrelu(x)
+#
+# plt.plot(x, y)
+# plt.axvline(x=0, color='orange', linestyle=':')
+# plt.title('Relu Function')
+# plt.show()
+
+
+# x = np.arange(-2.0, 2.0, 0.1)
+# y = np.exp(x) / np.sum(np.exp(x))
+#
+# plt.plot(x, y)
+# plt.title('Softmax Function')
+# plt.show()
+
+
+#############  숫자 필기 데이터
+# from sklearn.datasets import load_digits
+#
+# digits = load_digits()
+# # # print(digits.images[0])
+# # # print(digits.target[0])
+# # # print(len(digits.images))
+# #
+# #
+# # images_and_labels = list(zip(digits.images, digits.target))
+# # for index, (image,label) in enumerate(images_and_labels[:6]):
+# #     plt.subplot(2, 5, index+1)
+# #     plt.axis('off')
+# #     plt.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+# #     plt.title('Sample: %i'%index)
+# # plt.show()
+#
+#
+# x = torch.tensor(digits.data,dtype=torch.float32,requires_grad=True)
+# y = torch.tensor(digits.target,dtype=torch.int64)
+#
+# class Classifier(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.linear1 = nn.Linear(64,32)
+#         self.linear2 = nn.Linear(32,16)
+#         self.linear3 = nn.Linear(16,10)
+#
+#     def forward(self,x):
+#         x = F.relu(self.linear1(x))
+#         x = F.relu(self.linear2(x))
+#         return self.linear3(x)
+#
+# model = Classifier()
+# loss_fn = nn.CrossEntropyLoss()
+# optimizer = optim.Adam(model.parameters())
+#
+# losses = []
+#
+# for epoch in range(101):
+#     pred=model(x)
+#     loss = loss_fn(pred, y)
+#
+#     optimizer.zero_grad()
+#     loss.backward()
+#     optimizer.step()
+#
+#     if epoch % 10 ==0:
+#         print(f'Epoch : {epoch}  Cost : {loss.item():.6f}')
+#
+#     losses.append(loss.item())
+# plt.plot(losses)
+# plt.show()
+
+
+########### MNIST 데이터 분류하기
+# mnist_train = dsets.MNIST(root='MNIST_data/',
+#                           train=True,
+#                           transform=transforms.ToTensor(),
+#                           download=True)
+#
+# mnist_test = dsets.MNIST(root='MNIST_data/',
+#                          train=False,
+#                          transform=transforms.ToTensor(),
+#                          download=True)
+#
+#
+# loader_train = DataLoader(mnist_train, batch_size=64, shuffle=True)
+# loader_test = DataLoader(mnist_test, batch_size=64, shuffle=False)
+#
+#
+# class DNN(nn.Module):
+#     def __init__(self):
+#         super().__init__()
+#         self.linear1 = nn.Linear(784, 100)
+#         self.linear2 = nn.Linear(100, 100)
+#         self.linear3 = nn.Linear(100, 10)
+#
+#     def forward(self, x):
+#         x = F.relu(self.linear1(x))
+#         x = F.relu(self.linear2(x))
+#         return self.linear3(x)
+#
+#
+# model = DNN()
+# loss_fn = nn.CrossEntropyLoss()
+# optimizer = optim.Adam(model.parameters(), lr=0.01)
+#
+def train(epoch):
+    model.train()
+
+    for data, targets in loader_train:
+        data/=255
+        pred = model(data.view(-1,28*28))
+        loss = loss_fn(pred, targets)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f'Epoch{epoch} : 완료')
+
+def test():
+    model.eval()
+    correct = 0
+
+    with torch.no_grad():
+        for data, targets in loader_test:
+            data /= 255
+            output = model(data.view(-1,28*28))
+
+            pred = torch.argmax(output,dim=1)
+            correct += pred.eq(targets.data.view_as(pred)).sum()
+    data_num = len(loader_test.dataset)
+    print(f'정확도 : {correct/data_num*100:.2f}')
+
+for epoch in range(3):
+    train(epoch)
+test()
+
+model.eval()
+data = mnist_test.data[2018].float()
+output = model(data.view(-1,28*28))
+pred = torch.argmax(output)
+
+plt.imshow(mnist_test.data[2018].view(28,28),cmap='gray')
+plt.show()
+print(f'예측 : {pred}')
+print(f"정답 : {mnist_test.targets[2018]}")
+
+
+
+
+
+
